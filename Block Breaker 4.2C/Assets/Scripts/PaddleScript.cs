fd28f7b832219e5efad1d40001ef6d8ef7140af5
Shaun Zammit <b42475@mcast.org.mt>
2017-11-10 11:48:10 +0100

    -fixed mouse position from pixels to game units - set the ball to be locked to the paddle position - once user clicks mouse button, ball flies up to start the game - created surrounded borders

--- Block Breaker 4.2C/Assets/Scripts/PaddleScript.cs
+++ Block Breaker 4.2C/Assets/Scripts/PaddleScript.cs
@@ -13,9 +13,12 @@ public class PaddleScript : MonoBehaviour {
 	void Update () {
         print(Input.mousePosition.x);
 
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 16) - 8;
 
-        Vector3 newPaddlePos = new Vector3(Input.mousePosition.x, th.transform.position.y, this.transform.position.z);
-        
-       // this.transform.position = 
+        Vector3 newPaddlePos = new Vector3(mousePosInUnits, this.transform.position.y, this.transform.position.z);
+
+        newPaddlePos.x = Mathf.Clamp(mousePosInUnits, -7.5f, 7.5f);
+
+        this.transform.position = newPaddlePos;
 	}
 }
