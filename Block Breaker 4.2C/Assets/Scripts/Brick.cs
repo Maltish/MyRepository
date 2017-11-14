e4cb7978351b7e3d420aaa455b2c0ae2b0729d7e
Shaun Zammit <b42475@mcast.org.mt>
2017-11-14 12:17:17 +0100

    - Created brick prefabs for two different types of bricks - We set the whole game space as prefabs - We kept track of the bricks hit to destroy when maximum hits is reached - Changed the pixel per unit ratio for the brick and ball images

---
+++ Block Breaker 4.2C/Assets/Scripts/Brick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+    int timesHit;
+    public int maxHits;
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+        print(gameObject.name + " : " +timesHit);
+
+        if(timesHit >= maxHits)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
